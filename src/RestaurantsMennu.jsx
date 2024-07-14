2678e0708bf479efd87393cd099a21e4f714bf55
Anwarul07 <mdanwarul064@gmail.com>
2024-07-15 01:40:44 +0530

    upload RestaurantsMennu.jsx

--- src/RestaurantsMennu.jsx
+++ src/RestaurantsMennu.jsx
@@ -38,17 +38,27 @@ export const RestaurantsMennu = () => {
   return !restaurants ? (
     <Shimmer />
   ) : (
-    <div className="m-2 border-slate-600 border-2 w-60 bg-slate-300 h-[500px] justify-center  shadow-neutral-950 shadow-lg">
-      <img src={Cloudimg + data?.cloudinaryImageId} />
-      <h3 className="px-4 font-bold text-lg">{data?.name}</h3>
+    <>
+      <div className="m-2 border-slate-600 border-2 w-60 bg-slate-300 h-[500px] justify-center  shadow-neutral-950 shadow-lg">
+        <img src={Cloudimg + data?.cloudinaryImageId} />
+        <h3 className="px-4 font-bold text-lg">{data?.name}</h3>
 
-      <h3 className="px-4 font-semibold">{data?.costForTwo}</h3>
-      <h3 className="px-4 font-medium">{data?.avgRating} Star</h3>
-      <h3 className="px-4  font-normal">{data?.areaName}</h3>
-      <h3 className="px-4  font-normal">{data?.locality}</h3>
-      <h3 className="px-4  font-normal">{data?.sla?.slaString}</h3>
-      
-      <h3 className="px-4">Restaurants Id : {id}</h3>
-    </div>
+        <h3 className="px-4 font-semibold">{data?.costForTwo}</h3>
+        <h3 className="px-4 font-medium">{data?.avgRating} Star</h3>
+        <h3 className="px-4  font-normal">{data?.areaName}</h3>
+        <h3 className="px-4  font-normal">{data?.locality}</h3>
+        <h3 className="px-4  font-normal">{data?.sla?.slaString}</h3>
+
+        <h3 className="px-4">Restaurants Id : {id}</h3>
+      </div>
+      <div>
+        <h1 className="px-4">Menu</h1>
+        {/* <ul>
+          {Object.values(data?.menu?.items)?.map((item) => (
+            <li key={item?.card?.id}>{item?.name}</li>
+          ))}
+        </ul> */}
+      </div>
+    </>
   );
 };
